972be9bed56ff94a7f8cd6a12498db183eb7b6dc
Ivan-Malin <57503773+Ivan-Malin@users.noreply.github.com>
2026-09-20 21:39:44 +0300

    Added CICD checks for repo (#3)
    * Добавлена базовая настройка uv с ruff
    
    TODO - обавить корректный README.md
    
    * Добавлено описание в README.md
    
    * Исправлена очепятка
    
    * Обновлён визуал
    
    * Tasks/add strict ruff cicd checks (#2)
    
    * Testing actions
    
    * Added compliance-check for basic task
    
    * Added description for github actions
    
    * Added strict ruff repo check
    
    * Added ignoring of T201 to satisfy basic task rules
    
    * Removed copyright check (too much)
    
    * Fixed program to satisfy CICD checks
    
    ---------
    
    Co-authored-by: Ivan <way@way.way>

---
+++ catalog_analysis.py
@@ -0,0 +1,10 @@
+"""Анализ каталога фильмов."""
+
+
+def main() -> None:
+    """Точка входа программы."""
+    print("Hello from dz-catalog-analysis-malin-m26-555!")
+
+
+if __name__ == "__main__":
+    main()
